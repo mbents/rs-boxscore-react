e1d38548bd1d1ad5dcb2b96a99475d5a0368fb3c
mbents <mtbents@gmail.com>
2020-05-13 21:31:23 -0500

    add option to choose game and filter boxscores

--- src/components/Boxscore.tsx
+++ src/components/Boxscore.tsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react'
+import { Select, FormControl, InputLabel } from '@material-ui/core'
+import { IBoxscore } from '../models/IBoxscore'
 
 export const Boxscore = () => {
-  const [boxscore, setBoxscore] = useState({})
+  const [boxscore, setBoxscore] = useState<Array<IBoxscore>>([])
+  const [currentBoxscore, setCurrentBoxscore] = useState<IBoxscore>()
 
   useEffect(() => {
-    if (boxscore !== null) {
+    if (boxscore.length === 0) {
       const getBoxscore = async() => {
         await fetch('https://mbents.github.io/rs-data/games/2013BOS.json')
           .then(response => response.json())
@@ -16,7 +19,31 @@ export const Boxscore = () => {
     }
   })
 
+  const handleChange = (event: { currentTarget: any }) => {
+    if (event.currentTarget.value) {
+      const result = boxscore.filter(item => item.game_id === event.currentTarget.value)
+      console.log(result[0])
+      setCurrentBoxscore(result[0])
+    }
+  }
+
   return (
-    <h3>Hello boxscore</h3>
+    <React.Fragment>
+      <FormControl>
+        <InputLabel htmlFor="games">Games</InputLabel>
+        <Select
+          id="games"
+          native
+          label="Games"
+          autoWidth
+          onChange={handleChange}
+        >
+          <option value="" />
+          {boxscore.map(item =>
+            <option value={item.game_id}>{item.game_id}</option>
+          )}
+        </Select>
+      </FormControl>
+    </React.Fragment>
   )
 }
